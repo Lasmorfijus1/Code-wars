8b5a76da9a5e69164252683afa5bc91d37e912ff
Lasmorfijus1 <lasmorfijus@gmail.com>
2022-01-12 16:57:04 +0200

    A wolf in sheep's clothing

--- 8-kyu/app.js
+++ 8-kyu/app.js
@@ -1,1 +1,12 @@
-console.log('start'); 
+// If the wolf is the closest animal to you, return "Pls go away and stop eating my sheep". Otherwise, return 
+// "Oi! Sheep number N! You are about to be eaten by a wolf!" where N is the sheep's position in the queue.
+console.log('A wolf in sheeps clothing');
+
+function warnTheSheep(queue) {
+    if (queue[queue.length -1] === 'wolf') {
+      return 'Pls go away and stop eating my sheep';
+      } else {
+       let index = queue.findIndex( (x) => x == 'wolf' );
+       return `Oi! Sheep number ${queue.length - index - 1}! You are about to be eaten by a wolf!`;
+      }
+  }
